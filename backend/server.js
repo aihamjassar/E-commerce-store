42b6d021551d070f14612d549b7a9113a8250dff
Aiham Jassar <165441373+aihamjassar@users.noreply.github.com>
2026-09-15 20:53:15 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -33,8 +33,11 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+connectDB();
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
-  connectDB();
 });
+
+module.exports = app;
